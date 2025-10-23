83372e5ac81e88db9d61fd8efbd918bb8369d852
DarkFerrari <171889029+DarkFerrari@users.noreply.github.com>
2025-10-23 13:02:27 -0600

    update button active change
    button active blue color

--- src/index.js
+++ src/index.js
@@ -3,16 +3,40 @@ import {homeLoad} from "./homePage.js";
 import {menu} from "./menu.js";
 import {contact} from "./contact.js";   
 
-homeLoad();
-console.log('Hello, from restaurant index.js!');
-
 const homeButton = document.getElementById('home-btn');
 const menuButton = document.getElementById('menu-btn');
 const contactButton = document.getElementById('contact-btn');
+const buttons = [homeButton, menuButton, contactButton];
 
-homeButton.addEventListener('click', homeLoad);
-menuButton.addEventListener('click', menu);
-contactButton.addEventListener('click', contact);
+function clearActiveButton() {
+    buttons.forEach((button) => button.classList.remove('active'));
+}
+
+function showHome() {
+    clearActiveButton();
+    homeButton.classList.add('active');
+    homeLoad();
+}
+
+function showMenu() {
+    clearActiveButton();
+    menuButton.classList.add('active');
+    menu();
+}
+
+function showContact() {
+    clearActiveButton();
+    contactButton.classList.add('active');
+    contact();
+}
+
+
+homeLoad();
+console.log('Hello, from restaurant index.js!');
+
+homeButton.addEventListener('click', showHome);
+menuButton.addEventListener('click', showMenu);
+contactButton.addEventListener('click', showContact);
 
 
 
