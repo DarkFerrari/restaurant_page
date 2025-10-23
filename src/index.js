011cfa3536c5241036281af924cc37cf3597c12f
DarkFerrari <171889029+DarkFerrari@users.noreply.github.com>
2025-10-23 12:06:05 -0600

    center align text and image
    align text and image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./styles.css";
+import homeImage from "./assets/restaurant_home_image.png";
 
 console.log('Hello, from restaurant index.js!');
+
+const imageContainer = document.createElement('div');
+imageContainer.classList.add("image-container");
+document.body.appendChild(imageContainer);
+
+const image = document.createElement('img');
+image.src = homeImage;
+imageContainer.appendChild(image);
+image.classList.add("home-image");
+
+
